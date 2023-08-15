c0dab1af6fc688be789bdcfaaf6e163f2fb658dc
Maksim Razuvaev <maksim.razuvaev.wa@gmail.com>
2023-08-14 21:08:55 -0700

    MakeCSV() file function is written to save user data input to CSV file in Util.js

--- CatWorx.BadgeMaker/Util.cs
+++ CatWorx.BadgeMaker/Util.cs
@@ -1,4 +1,5 @@
 using System;
+using System.IO;
 using System.Collections.Generic;
 
 
@@ -14,6 +15,27 @@ namespace CatWorx.BadgeMaker
         {
             string template = "{0,-10}\t{1,-20}\t{2}";
             Console.WriteLine(String.Format(template, employees[i].GetId(), employees[i].GetFullName(), employees[i].GetPhotoUrl()));
+        }
+    }
+
+    //Method to save data to CSV file
+        public static void MakeCSV(List<Employee> employees)
+    {
+        // Check to see if folder exists
+        if (!Directory.Exists("data")) 
+        {
+          // If not, create it
+          Directory.CreateDirectory("data");
+        }
+        using (StreamWriter file = new StreamWriter("data/employees.csv"))
+        {
+            // Loop over employees
+          for (int i = 0; i < employees.Count; i++)
+          {
+            // Write each employee to the file
+            string template = "{0},{1},{2}";
+            file.WriteLine(String.Format(template, employees[i].GetId(), employees[i].GetFullName(), employees[i].GetPhotoUrl()));
+          }
         }
     }
   }
