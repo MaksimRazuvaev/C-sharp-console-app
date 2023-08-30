6751111b6efa277b4931bdb6b3d20fd3f01c9c5d
Maksim Razuvaev <maksim.razuvaev.wa@gmail.com>
2023-08-30 09:58:09 -0700

    Program.cs file both GetEmployees() and GetFromApi() methods are implemented

--- CatWorx.BadgeMaker/Program.cs
+++ CatWorx.BadgeMaker/Program.cs
@@ -8,8 +8,22 @@ namespace CatWorx.BadgeMaker
   {
     async static Task Main(string[] args)
     {
-      // List<Employee> employees = PeopleFetcher.GetEmployees();
-      List<Employee> employees = await PeopleFetcher.GetFromApi();
+
+      List<Employee> employees = new List<Employee>();
+
+      Console.WriteLine("Welcome to the CatWorx BadgeMaker Program!");
+      Console.WriteLine("Do you want enter employees manually? 'Y' or 'N'");
+      string answer = Console.ReadLine() ?? "";
+      if (answer == "y" || answer == "Y")
+      {
+        employees = PeopleFetcher.GetEmployees();
+      }
+      else
+      {
+        Console.WriteLine("Here are random employees from API");
+        employees = await PeopleFetcher.GetFromApi();
+      }
+
       Util.PrintEmployees(employees);
       Util.MakeCSV(employees);
       await Util.MakeBadges(employees);
